53aafb25bc17f028a1833f877a3adad44ae74c82
thaohienhomes <thaohienhomes@gmail.com>
2025-09-10 16:14:59 +0700

    convex: export orders/payos/reconcile/users from index so production has public functions (fix orders.listRecent not found)

--- pho-chat/convex/index.ts
+++ pho-chat/convex/index.ts
@@ -1,3 +1,8 @@
-import { query, mutation, internalMutation, internalQuery } from "./_generated/server";
-export { query, mutation, internalMutation, internalQuery };
+import { query, mutation, internalMutation, internalQuery, action } from "./_generated/server";
+export { query, mutation, internalMutation, internalQuery, action };
 
+// Export domain functions so Convex can deploy them
+export * as orders from "./orders";
+export * as payos from "./payos";
+export * as reconcile from "./reconcile";
+export * as users from "./users";
